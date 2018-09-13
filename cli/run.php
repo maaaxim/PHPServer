488943d1999405aab31047b4c522ed488862a494
Maxim Loboda <maxim.loboda@gmail.com>
2018-09-13 21:49:47 +0300

    add shebang

--- cli/run.php
+++ cli/run.php
@@ -1,10 +1,11 @@
+#!/usr/bin/env php
 <?php
 
 use Maaaxim\PHPServer\Request;
 use Maaaxim\PHPServer\Response;
 use Maaaxim\PHPServer\Server;
 
-require_once 'vendor/autoload.php';
+require_once './vendor/autoload.php';
 
 $server = new Server('localhost', 8888);
 $server->listen(function (Request $request) {
